c4f04c636c8455c01f50785a85ccc5702a9988ce
Documenter.jl <documenter@juliadocs.github.io>
2023-09-26 18:01:42 +0000

    build based on bd24fed

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ACEluxpots","category":"page"},{"location":"#ACEluxpots","page":"Home","title":"ACEluxpots","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ACEluxpots.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ACEluxpots]","category":"page"}]
+}
