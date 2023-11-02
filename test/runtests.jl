e02f278174186c9eb052a3d9ddefd54cf44cebc8
YangshuaiWang <yswang2016@sjtu.edu.cn>
2023-11-01 21:22:53 -0700

    fix some tiny bugs

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,10 @@ using Test
 
 @testset "ACEluxpots.jl" begin
 
-    @testset "Test potential" begin include("test_potential.jl") end
-    @testset "Test calculator" begin include("test_calculator.jl") end
-    @testset "Test Optimization of W" begin include("test_W_optim.jl") end
-    @testset "Tets Optimization of NiAl" begin include("test_NiAl_optim.jl") end
+    # @testset "Test potential" begin include("test_potential.jl") end
+    # @testset "Test calculator" begin include("test_calculator.jl") end
+    @testset "Test Profile" begin include("test_profile.jl") end
+    # @testset "Test Optimization of W" begin include("test_W_optim.jl") end
+    # @testset "Tets Optimization of NiAl" begin include("test_NiAl_optim.jl") end
 
 end
--- test/test_profile.jl
+++ test/test_profile.jl
@@ -10,6 +10,19 @@ using ACEluxpots: Pot_single
 using ACEluxpots
 using ProfileView, BenchmarkTools
 
+# dataset
+using ASE, JuLIP
+function gen_dat()
+   eam = JuLIP.Potentials.EAM("../examples/potentials/w_eam4.fs")
+   at = rattle!(bulk(:W, cubic=true) * 2, 0.1)
+   set_data!(at, "energy", energy(eam, at))
+   set_data!(at, "forces", forces(eam, at))
+   set_data!(at, "virial", virial(eam, at))
+   return at
+end
+Random.seed!(0)
+train = [gen_dat() for _ = 1:10];
+
 rcut = 5.5 
 maxL = 0
 totdeg = 8
@@ -28,6 +41,21 @@ X = [ @SVector(randn(3)) for i in 1:10 ]
 model(X, ps, st)
 Zygote.gradient(X -> model(X, ps, st)[1], X)[1]
 
+function loss(train, calc, p_vec)
+   ps = _rest(p_vec)
+   st = calc.st
+   err = 0
+   for at in train
+      Nat = length(at)
+      Eref = at.data["energy"].data
+      Fref = at.data["forces"].data
+      Vref = at.data["virial"].data
+      E, F, V = Pot_single.lux_efv(at, calc, ps, st)
+      err += ( (Eref-E) / Nat)^2 + sum( f -> sum(abs2, f), (Fref .- F) ) / Nat / 100  #  + 
+         # sum(abs2, (Vref.-V) )
+   end
+   return err
+end
 
 Zygote.gradient(p -> model(X, p, st)[1], ps)[1]
 ReverseDiff.gradient(p -> loss(train, calc, p), p_vec)
